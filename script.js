d337410a61f11681d34f718bb0ec55cbe04957ba
Frogboy <nickbarstatis@yahoo.com>
2014-07-06 13:59:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-SC.initialize({
-  client_id: 'YOUR_CLIENT_ID'
+function playSomeMusic(myGenre) {  
+
+}
+
+SC.initialize({      
+  client_id: 'e7980ff6a02bf037006a6da30df98fd8'
+}
 });
 
-$(document).ready(function() {
-SC.get('/tracks/293', function(track) {
-SC.oEmbed(track.permalink_url, document.getElementById('player'));
-});
+SC.get('/tracks', { genres: myGenre, bpm: { from: 60 } },
+  function(tracks) {
+    //FIRST soundcloud sends back an array of 50 objects (0~49)
+    //so let's randomize which one we play:
+      //this logic returns a number between 0 and 50
+      var random = Math.floor(Math.random() * 49);
+      var track_url = tracks[random].permalink_url;
+      //LAST let's use the oEmbed method to display the soundplayer
+      //in the element #target with a options such as auto play
+      SC.oEmbed(track_url, {auto_play: true, color: "ff0066"},
+        document.getElementById("target"));
+      }
+);
